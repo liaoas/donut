0d8b740ef10334958212a83bedffa46685785ce3
liao991224@gmail.com <li991224ao>
2020-02-27 23:02:23 +0800

    pop up

---
+++ main/java/com/liao/hello/Hello.java
@@ -0,0 +1,8 @@
+package com.liao.hello;
+
+public class Hello {
+
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
--- main/resources/static/js/usre.js
+++ main/resources/static/js/usre.js
@@ -1,31 +1,55 @@
 window.onload = function () {
-    axios.default.baseURL = '';
+    // 基准地址
+    axios.defaults.baseURL = 'http://localhost:8080/donut/';
+    axios.inter
     var table = new Vue({
         el: '#user_table',
         data: {
             user: '',
             user1: 'asdasd'
         },
-        computed: {
-            createcode() {
-                var userData = '';
-                $.ajax({
-                    url: 'http://localhost:8080/donut/user/selectall',
-                    dataType: 'json',
-                    type: 'post',
-                    data: {},
-                    success: function (datas) {
-                        userData = datas.data.list;
-                        console.log(userData);
+        methods:{
+            queryDate :async function () {
+                var ret = await axios.post('user/selectall');
+                this.user = ret.data.list;
+            },
+            popup:function () {
+                // 弹出框事件
+                $('#item').popup({
+                    time: 1000,
+                    classAnimateShow: 'flipInX',
+                    classAnimateHide: 'hinge',
+                    onPopupClose: function e() {
+                        // console.log('0')
+                    },
+                    onPopupInit: function e() {
+                        // console.log('1')
                     }
-                })
-                return userData;
+                });
             }
         },
-        methods: {
-            add() {
-                alert(this.user)
+        computed: {
+        },
+        filters:{
+            date(time){
+                let date   = new Date(time)//把定义的时间赋值进来进行下面的转换
+                let year   = date.getFullYear();
+                let month  = date.getMonth()+1;
+                let day    = date.getDate();
+                let hour   = date.getHours();
+                let minute = date.getMinutes();
+                let second = date.getSeconds();
+                return year+"-"+month+"-"+day+" "+hour+":"+minute+":"+second;
             }
+        },
+        mounted:function () {
+            // axios.post("user/selectall").then( (res) => {
+            //     this.user = res.data.list;
+            //     console.log("===================")
+            //     console.log(that.user);
+            // })
+
+            this.queryDate();
         }
     })
 }
